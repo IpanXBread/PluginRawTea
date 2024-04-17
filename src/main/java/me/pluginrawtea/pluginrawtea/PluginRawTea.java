b83bb4fb71bd2ec877e267f43b850244b3fb3046
IpanXBread <ipanxbread@gmail.com>
2024-04-17 12:49:00 +0800

    Initial commit-1 by ipanxbread

--- src/main/java/me/pluginrawtea/pluginrawtea/PluginRawTea.java
+++ src/main/java/me/pluginrawtea/pluginrawtea/PluginRawTea.java
@@ -1,17 +1,23 @@
 package me.pluginrawtea.pluginrawtea;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PluginRawTea extends JavaPlugin {
+public final class PluginRawTea extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Ipan said hello");
+        getServer().getPluginManager().registerEvents(this, this);
+        //test 22
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        System.out.println("ipan said welcome to you :)");
     }
+
 }
